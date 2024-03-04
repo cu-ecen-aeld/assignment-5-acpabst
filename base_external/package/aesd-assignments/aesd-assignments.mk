def70381f9b66d1ae9c5ea96943bedc8158d84e2
Andy Pabst <andy.pabst16@gmail.com>
2024-03-04 06:03:26 +0000

    removed bianry, assignment 3 git hash updated

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = '41772616bfa959e786e533f0a57afc45bf6f972d'
+AESD_ASSIGNMENTS_VERSION = 'c58e39d710a985ab38d57091825b62ec8db632d2'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
